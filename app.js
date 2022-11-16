d6541bbff122d44cc2caede77374d340adc7a51b
Shabbir-Hussain-07 <2019n00032@gmail.com>
2022-11-16 22:09:39 +0500

    connected database+basic authentication

--- app.js
+++ app.js
@@ -6,16 +6,29 @@ const helmet = require("helmet");
 const ejs = require("ejs");
 const _= require("lodash");
 const cookieParser = require('cookie-parser')
+const db = require("./db_config/db");
+const auth = require("./authorization/auth.js");
+const session = require('express-session');
+const passport = require("passport");
 
+// authorization\auth.js
 const app = express();
 
 const port = process.env.PORT || 5000
 
+
+db.connect((err) => {
+  if(err){
+    console.log(err);
+  } 
+  else{
+    console.log("DATABASE CONNECTED")
+  }
+})
+
 app.set('view engine','ejs');
-
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended:true}));
 const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
 const { runInNewContext } = require("vm");
 const { concat } = require("lodash");
@@ -23,27 +36,43 @@ const { start } = require("repl");
 
 
 app.get("/main",(req,res)=>{
-  res.render("main"); // file name original 
+  res.render("main", {message: ""}); // file name original 
 });
- 
+
 app.get("/home",(req,res)=>{
-  res.render("home");
-});
+  console.log(req);
+  res.render("home", {message: ""}); // file name original 
+}
+);
 
 app.get("/",(req,res)=>{
-    res.render("home");
+    res.render("main", {message: ""});
 })
 
-app.post("/home",(req,res)=>{
+app.post("/main",(req,res)=>{
+  username = req.body.userID;
+  password = req.body.password;
+  console.log(username);
+  console.log(password);
+  auth.login(username,password, res);
+}
+);
 
-  userName = req.body.userID;
-  password = req.body.password
+// write above code as call back function
 
-  console.log(userName, password)
-
-  res.redirect("/main");
-
+app.get("/create_account",(req,res)=>{
+  res.render("create_account", {message: ""}); // file name original 
 });
+
+app.post("/create_account",(req,res)=>{
+  username = req.body.userID;
+  password = req.body.password;
+  email = req.body.email;
+  role = req.body.role;
+  auth.createAccount(username,password,email,role, res);
+}
+);
+
 
 
 app.listen(5000,()=>{
