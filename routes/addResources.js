7ae3c3b034cd6d32f3e3261252c5658787a018a1
daimakram <85197435+daimakram@users.noreply.github.com>
2022-12-09 11:53:39 +0500

    Update addResources.js

--- routes/addResources.js
+++ routes/addResources.js
@@ -40,7 +40,7 @@ const uploadResource = async (course_id, resource_type,year_offered, sem_offered
         var params = {
             Key : resource_key,
             Body : file.data,
-            Bucket: "cyclic-thoughtful-long-underwear-duck-eu-central-1",
+            Bucket: "cyclic-sore-erin-perch-shoe-eu-west-3",
             ContentType : file.mimetype //filetype pdf/docx/xls etc
         }
     
