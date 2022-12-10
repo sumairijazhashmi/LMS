74767f4e1bcc80304d8dbde0f4111ba96aaaecaa
Sumair Ijaz Hashmi <85024854+sumairijazhashmi@users.noreply.github.com>
2022-12-10 08:59:49 +0500

    fix view assignment pls work

--- routes/assignmentsTab.js
+++ routes/assignmentsTab.js
@@ -27,12 +27,59 @@ function seedData(query)
       });
 }
 
-const assignmentsTab = async (courseID, year, sem) => {
-    let query = `select topic, released_date, due_date from Assessments where course_ID = ${courseID} and year_offered = ${year} and sem_offered = "${sem}";`;
+const assignmentsTab = async (courseID, year, sem, res) => {
+    let query = `select * from Assessments where course_ID = ${courseID} and year_offered = ${year} and sem_offered = "${sem}";`;
     let x = await seedData(query);
+    console.log("did i reach here\n");
+    try{ //if result has some files
+        // console.log("123", typeof x);
+        console.log(x[0].ass_key)
+        res.render("assignmentsTab", {
+        status:"display: block",
+        noRecords:"display: none",
+        data:x
+      });
+      }
+      catch //if result is empty
+      { 
+        console.log("no records")
+        res.render("assignmentsTab", {
+        status:"display: none",
+        noRecords:"display: block",
+        data:""
+      });
+      }
     return x;
 }
 
+async function manageFile(filepath,res,s3)
+{
+  //parameters to pass in fucntion
+  const s3Params = {
+    Bucket: "cyclic-thoughtful-long-underwear-duck-eu-central-1", //aws bucket name - 'DON'T CHNAGE IT
+    Key: filepath + '' //FILE-Key
+  };
+
+  console.log("viewing");
+ 
+
+  s3.getSignedUrl('getObject', s3Params, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.end();
+      }
+      const returnData = {
+          signedRequest: data,
+      };
+      x=returnData["signedRequest"]
+      //console.log(x)
+      res.writeHead(302, {
+        'Location': x
+      });
+      res.end();
+    });
+}
+
 module.exports = {
-    assignmentsTab
+    assignmentsTab, manageFile
 }
